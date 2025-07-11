2ca264956102e09e5ee93d75fb96db1d4b9da1b3
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-07-11 23:57:52 +0600

    blog pipeline test 7

--- blog/bloglist-frontend/src/components/Blog.jsx
+++ blog/bloglist-frontend/src/components/Blog.jsx
@@ -1,60 +1,76 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Togglable from './Togglable'
-import blogService from '../services/blogs'
+import Togglable from "./Togglable";
+import blogService from "../services/blogs";
 const Blog = ({ blog, token, blogRefresh, user }) => {
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
-    border: 'solid',
+    border: "solid",
     borderWidth: 1,
-    marginBottom: 5
-  }
-  const [likes, setLikes] = useState(blog.likes)
-  const [visible, setVisible] = useState(true)
+    marginBottom: 5,
+  };
+  const [likes, setLikes] = useState(blog.likes);
+  const [visible, setVisible] = useState(true);
   const handleLikes = async () => {
-    blog.likes += 1
-    if (token){
-      const updatedBlog = await blogService.addLike(token, blog)
-      setLikes(updatedBlog.likes)
-      blogRefresh()
+    blog.likes += 1;
+    if (token) {
+      const updatedBlog = await blogService.addLike(token, blog);
+      setLikes(updatedBlog.likes);
+      blogRefresh();
     } else {
-      blogRefresh()
+      blogRefresh();
     }
-  }
+  };
 
   const handleDelete = async () => {
-    if(window.confirm(`Remove blog ${blog.title} by ${blog.author}`))
-      await blogService.deleteBlog(token, blog.id)
-    setVisible(false)
-  }
+    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`))
+      await blogService.deleteBlog(token, blog.id);
+    setVisible(false);
+  };
 
-  if(visible){
+  if (visible && user) {
     return (
       <div id="blog_title_blog_author" className="entry" style={blogStyle}>
-        {blog.title} {blog.author} <Togglable buttonLabel="view" closeButtonLabel="hide">
-          <div id="blog_url">
-            {blog.url}
-          </div>
+        {blog.title} {blog.author}{" "}
+        <Togglable buttonLabel="view" closeButtonLabel="hide">
+          <div id="blog_url">{blog.url}</div>
           <div id={`${blog.title}_likes`}>
-        likes {likes}
-            <button id={`${blog.title}-like-button`} onClick={handleLikes}>like</button>
+            likes {likes}
+            <button id={`${blog.title}-like-button`} onClick={handleLikes}>
+              like
+            </button>
           </div>
-          {blog.user.username!==user.username ? null :
+          {blog.user.username !== user.username ? null : (
             <div id="blog_delete">
-              <button id="delete-button" onClick={handleDelete}>delete</button>
-            </div>}
-          <div id="blog_user">
-            {!blog.user ? '' : blog.user.username}
-          </div>
+              <button id="delete-button" onClick={handleDelete}>
+                delete
+              </button>
+            </div>
+          )}
+          <div id="blog_user">{!blog.user ? "" : blog.user.username}</div>
         </Togglable>
       </div>
-    )}
-  else{
-    return(
-      <>
-      </>
-    )
-  }}
+    );
+  } else if (visible) {
+    return (
+      <div id="blog_title_blog_author" className="entry" style={blogStyle}>
+        {blog.title} {blog.author}{" "}
+        <Togglable buttonLabel="view" closeButtonLabel="hide">
+          <div id="blog_url">{blog.url}</div>
+          <div id={`${blog.title}_likes`}>
+            likes {likes}
+            <button id={`${blog.title}-like-button`} onClick={handleLikes}>
+              like
+            </button>
+          </div>
+          <div id="blog_user">{!blog.user ? "" : blog.user.username}</div>
+        </Togglable>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+};
 
-export default Blog
+export default Blog;
